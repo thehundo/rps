be34ea83919a555313a45f3d7f720d72cba39c34
Brandon Hughes <157261373+thehundo@users.noreply.github.com>
2024-03-11 17:23:46 -0400

    Create constants for playRound

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function getComputerSelection() {
 // Create a variable randomNumber between 0 - 2
     let randomNumber = Math.floor(Math.random()*3);
-    console.log(randomNumber);
+    // console.log(randomNumber);
 // Set getComputerSelection to "rock" (0), "paper" (1) or "scissors"(2)
     if (randomNumber == 0) {
         return "rock";
@@ -15,13 +15,13 @@ function getComputerSelection() {
         }
 }
 // SHOW getComputerSelection in console
-console.log("getComputerSelection: " + getComputerSelection());
+// console.log("getComputerSelection: " + getComputerSelection());
 
 // CREATE a function getUserSelection
 function getUserSelection() {
 // ASK User to choose "Rock" "Paper" or "Scissors"
     let userSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors'").toLowerCase();
-// CHECK that answer is valid and return "invalid response" if not
+// CHECK that answer is valid and return "idiot" if not
     if (userSelection == "rock" || 
         userSelection == "paper" ||
         userSelection == "scissors") {
@@ -30,10 +30,26 @@ function getUserSelection() {
         else {
             return "idiot";
     } ;
-// Convert answer to Capitalized (how?)
-
-// Return capitalized answer to getUserSelection function
-
 }
 // SHOW getUserSelection
-console.log("getUserSelection: " + getUserSelection());
+// console.log("getUserSelection: " + getUserSelection());
+
+
+// CREATE a constant userSelection
+const userSelection = getUserSelection();
+console.log("user: " + userSelection);
+// CREATE a constant computerSelection
+const computerSelection = getComputerSelection();
+console.log("computer: " + computerSelection);
+// CREATE a function playRound with arguments userSelection and computerSelection
+function playRound(userSelection, computerSelection) {
+// Check for tie: if userSelection is the same as computerSelection
+    if (userSelection == computerSelection) {
+        return alert("you tied.")
+    }
+//      Display "You Tied" and userSelection == computerSelection
+// Check for win
+//      Display "You Won" and UserSelection beats computerSelection
+// Since user did not tie or win, Display "You Lost" and ComputerSelection beats UserSelection
+// 
+}
