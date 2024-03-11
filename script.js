bd40710667147709635a17fb0b81764ae3b54a2d
Brandon Hughes <157261373+thehundo@users.noreply.github.com>
2024-03-11 15:01:48 -0400

    build and debug getUserSelection function

--- script.js
+++ script.js
@@ -3,17 +3,37 @@ function getComputerSelection() {
 // Create a variable randomNumber between 0 - 2
     let randomNumber = Math.floor(Math.random()*3);
     console.log(randomNumber);
-// Set getComputerSelection to "Rock" (0), "Paper" (1) or "Scissors"(2)
+// Set getComputerSelection to "rock" (0), "paper" (1) or "scissors"(2)
     if (randomNumber == 0) {
-        return "Rock";
-    }
+        return "rock";
+        }
         else if (randomNumber == 1) {
-            return "Paper";
+            return "paper";
         }
         else {
-            return "Scissors";
+            return "scissors";
         }
+}
 // SHOW getComputerSelection in console
-            console.log(getComputerSelection());
-    }
+console.log("getComputerSelection: " + getComputerSelection());
 
+// CREATE a function getUserSelection
+function getUserSelection() {
+// ASK User to choose "Rock" "Paper" or "Scissors"
+    let userSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors'").toLowerCase();
+// CHECK that answer is valid and return "invalid response" if not
+    if (userSelection == "rock" || 
+        userSelection == "paper" ||
+        userSelection == "scissors") {
+        return userSelection;
+    }
+        else {
+            return "idiot";
+    } ;
+// Convert answer to Capitalized (how?)
+
+// Return capitalized answer to getUserSelection function
+
+}
+// SHOW getUserSelection
+console.log("getUserSelection: " + getUserSelection());
